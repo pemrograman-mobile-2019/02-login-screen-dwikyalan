b09f42b106e0d9e062218e42cd518f2a22412d1e
Dwiky Alan <dwikyalanadi@gmail.com>
2019-02-20 21:47:15 +0700

    login-screen

--- app/src/main/java/com/example/putraprima/myapplication/MainActivity.java
+++ app/src/main/java/com/example/putraprima/myapplication/MainActivity.java
@@ -12,39 +12,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getApplicationContext(),"OnRestart",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(),"OnStart",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(),"OnResume",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(),"OnPause",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(),"OnStop",Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(),"OnDestroy",Toast.LENGTH_SHORT).show();
-    }
+//    @Override
+//    protected void onRestart() {
+//        super.onRestart();
+//        Toast.makeText(getApplicationContext(),"OnRestart",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        Toast.makeText(getApplicationContext(),"OnStart",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        Toast.makeText(getApplicationContext(),"OnResume",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        Toast.makeText(getApplicationContext(),"OnPause",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        Toast.makeText(getApplicationContext(),"OnStop",Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        Toast.makeText(getApplicationContext(),"OnDestroy",Toast.LENGTH_SHORT).show();
+//    }
 }
